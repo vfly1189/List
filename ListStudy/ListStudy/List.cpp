d05ad73f747e4c52bd9f6699265566668892dc47
Vfly <vfly1189@naver.com>
2020-12-08 21:49:20 +0900

    구조체 리스트 삭제 검색 추가 리스트 출력 완성

--- ListStudy/ListStudy/List.cpp
+++ ListStudy/ListStudy/List.cpp
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include <windows.h>
 #include <stdlib.h>
+
+int list_length = 0;
 
 //리스트를 구성할 노드
 typedef struct _node
@@ -69,13 +72,62 @@ Node *add_node_upper(Node* head, Node* new_node)
 //리스트에 존재하는 데이터(나이)를 출력
 void show_list(Node* head)
 {
-	while (head != NULL)
+	for (int i = 1; head != NULL; head = head->next)
 	{
-		printf("%d ", head->age);
-		head = head->next;
+		printf("%d : %d\n", i++, head->age);
 	}
 	return;
 }
+
+//리스트에 원하는 레코드가 있는지 검색
+bool search_data(Node *head, int age)
+{
+	//0은 아직 못 찾음, 1은 찾음
+	int flag = 0;
+	for (; head != NULL; head = head->next)
+	{
+		if (head->age == age)
+		{
+			flag = 1; break;
+		}
+	}
+	if (flag == 1) return true;
+	else return false;
+}
+
+//리스트에서 레코드 삭제
+Node* delete_data(Node* head, int record_num)
+{
+	Node* prev = NULL;
+	Node* move = head;
+	for (int i = 0; i < record_num - 1; i++)
+	{
+		prev = move;
+		move = move->next;
+	}
+	//맨 처음 레코드를 삭제할때
+	if (move == head)
+	{
+		head = move->next;
+		move->next = NULL;
+		free(move);
+	}
+	//중간단계 레코드를 삭제할때
+	else if (move->next == NULL)
+	{
+		prev->next = NULL;
+		free(move);
+	}
+	//맨끝 레코드를 삭제할때
+	else
+	{
+		prev->next = move->next;
+		move->next = NULL;
+		free(move);
+	}
+	return head;
+}
+
 
 int main()
 {
@@ -85,19 +137,65 @@ int main()
 	while (1)
 	{
 		int select = 0;
-	
-		printf("나이를 입력하세요. 양수를 입력하지 않으면 종료됩니다.\n");
+		system("cls");
+		printf("List Test Program\n");
+		printf("1. add   2. search   3. delete   4. show\n");
 		scanf("%d", &select);
 
-		//양수가 아닐경우 종료
-		if (select < 1)
-			break;
-		else
+		//리스트에 추가
+		if (select == 1)
 		{
-			Node* new_node = create_node(select);
+			int age;
+			printf("나이를 입력하세요 : "); 
+			scanf("%d", &age);
+			Node* new_node = create_node(age);
 			head = add_node_upper(head, new_node);
+			printf("\ncomplete\n\n");
+			list_length++;
+		
+			getchar();
+			getchar();
+		}
+		//리스트에서 검색
+		else if (select == 2)
+		{
+			int want_search;
+			printf("찾고자 하는 나이를 입력하세요 : ");
+			scanf("%d", &want_search);
+
+			//리스트에 존재하면 true 없으면 false를 리턴함
+			if (search_data(head, want_search)) printf("리스트에 존재합니다\n\n");
+			else printf("리스트에 존재하지 않습니다\n\n");
+			
+			//1초 딜레이
+			Sleep(1000);
+		}
+		//리스트에서 삭제
+		else if (select == 3)
+		{
 			show_list(head);
-			printf("\n");
+
+			int record_num;
+			printf("삭제하고자 하는 레코드의 번호를 입력하세요 : ");
+			scanf("%d", &record_num);
+
+			if (record_num > list_length)
+			{
+				printf("\n다시 입력하세요.\n");
+				Sleep(1000);
+			}
+
+			head = delete_data(head, record_num);
+		}
+		//리스트 전체 출력
+		else if (select == 4)
+		{
+			show_list(head);
+			printf("\n엔터를 입력하세요\n");
+			//버퍼에 남아있는 엔터받아드림
+			getchar();
+			//아무키 입력이나 들어올때 까지 대기
+			getchar();
 		}
 	}
 
